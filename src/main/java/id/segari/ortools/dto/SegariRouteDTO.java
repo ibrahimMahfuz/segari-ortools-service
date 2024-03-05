f39a9eca6d3b2452f3b0fdf8ae62d6c8fed4e7e1
Mahfuz Jailani Ibrahim <mahfuz.ibrahim@segari.id>
2024-03-05 13:24:21 +0700

    add validation

--- src/main/java/id/segari/ortools/dto/SegariRouteDTO.java
+++ src/main/java/id/segari/ortools/dto/SegariRouteDTO.java
@@ -1,5 +1,8 @@
 package id.segari.ortools.dto;
 
+import id.segari.ortools.validation.group.TspFixStartArbitraryFinish;
+import id.segari.ortools.validation.group.VrpArbitraryStartArbitraryFinish;
+import id.segari.ortools.validation.group.VrpSpStartArbitraryFinish;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -13,12 +16,12 @@ import java.util.List;
 @Setter
 @Builder
 public class SegariRouteDTO {
-    @NotEmpty
-    private List<@NotNull SegariRouteOrderDTO> orders;
-    @NotNull
-    @Min(1)
+    @NotEmpty(groups = {VrpSpStartArbitraryFinish.class, VrpArbitraryStartArbitraryFinish.class, TspFixStartArbitraryFinish.class})
+    private List<@NotNull(groups = {VrpSpStartArbitraryFinish.class, VrpArbitraryStartArbitraryFinish.class, TspFixStartArbitraryFinish.class}) SegariRouteOrderDTO> orders;
+    @NotNull(groups = {VrpSpStartArbitraryFinish.class, VrpArbitraryStartArbitraryFinish.class, TspFixStartArbitraryFinish.class})
+    @Min(value = 1, groups = {VrpSpStartArbitraryFinish.class, VrpArbitraryStartArbitraryFinish.class, TspFixStartArbitraryFinish.class})
     private Integer maxTotalDistanceInMeter;
-    @NotNull
-    @Min(1)
+    @NotNull(groups = {VrpSpStartArbitraryFinish.class, VrpArbitraryStartArbitraryFinish.class, TspFixStartArbitraryFinish.class})
+    @Min(value = 1, groups = {VrpSpStartArbitraryFinish.class, VrpArbitraryStartArbitraryFinish.class, TspFixStartArbitraryFinish.class})
     private Integer maxOrderCount;
 }
--- src/main/java/id/segari/ortools/dto/SegariRouteOrderDTO.java
+++ src/main/java/id/segari/ortools/dto/SegariRouteOrderDTO.java
@@ -1,5 +1,8 @@
 package id.segari.ortools.dto;
 
+import id.segari.ortools.validation.group.TspFixStartArbitraryFinish;
+import id.segari.ortools.validation.group.VrpArbitraryStartArbitraryFinish;
+import id.segari.ortools.validation.group.VrpSpStartArbitraryFinish;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -7,17 +10,17 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class SegariRouteOrderDTO {
-    @NotNull
+    @NotNull(groups = {VrpSpStartArbitraryFinish.class, VrpArbitraryStartArbitraryFinish.class, TspFixStartArbitraryFinish.class})
     private final Long id;
-    @NotNull
+    @NotNull(groups = {VrpSpStartArbitraryFinish.class, VrpArbitraryStartArbitraryFinish.class, TspFixStartArbitraryFinish.class})
     private final SegariRouteOrderEnum type;
     private final Double latitude;
     private final Double longitude;
-    @NotNull
+    @NotNull(groups = {VrpSpStartArbitraryFinish.class, VrpArbitraryStartArbitraryFinish.class, TspFixStartArbitraryFinish.class})
     private final Boolean isExtension;
-    @NotNull
+    @NotNull(groups = {VrpSpStartArbitraryFinish.class, VrpArbitraryStartArbitraryFinish.class, TspFixStartArbitraryFinish.class})
     private final Boolean isTurbo;
-    @NotNull
+    @NotNull(groups = {VrpSpStartArbitraryFinish.class, VrpArbitraryStartArbitraryFinish.class, TspFixStartArbitraryFinish.class})
     private final Boolean isInstan;
 
 
